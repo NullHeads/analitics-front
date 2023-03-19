2f17ac7b28c3069b38995ed6ad02d868b184b814
IvanovDevelopment <victorivanovrabota19@gmail.com>
2023-03-19 16:43:44 +0300

    исправил лейблы

--- src/components/CustomBar.js/CustomBar.jsx
+++ src/components/CustomBar.js/CustomBar.jsx
@@ -11,35 +11,47 @@ import { Bar } from 'react-chartjs-2';
 import "./CustomBar.css"
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-  );
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
   
-  export const options = {
-    indexAxis: 'y',
-    elements: {
-      bar: {
-        borderWidth: 2,
-      },
+const ruCriterions = {
+  teamMembers: "Количество сотрудников в команде",
+  dayAvTime: "Среднее время работы",
+  taskAmount: "Количество задач",
+  ncomplitedTask: "Количество невыполненных задач",
+  avgMess: "Среднее количество сообщений",
+  projectsAmount: "Количество проектов",
+  stekAmount: "Количество технологий",
+  sportPlayer: "Занятия спортом"
+}
+
+export const options = {
+  indexAxis: 'y',
+  elements: {
+    bar: {
+      borderWidth: 2,
     },
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'right',
-      },
-      title: {
-        display: true,
-        text: 'Показатели сотрудника',
-      },
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'right',
     },
-  };  
+    title: {
+      display: true,
+      text: 'Показатели сотрудника',
+    },
+  },
+};  
  
 export const CustomBar= ({analyticsData}) => {
   const labels = Object.keys(analyticsData)
+    .map(label => ruCriterions[label])
   const dataset = Object.values(analyticsData)
 
     const data = {
